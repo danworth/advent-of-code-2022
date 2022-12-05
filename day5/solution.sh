38dc5930fbcb9e486c0c96ca7965c16e2488f007
Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
2022-12-05 11:16:36 +0000

    working day 5

--- day5/solution.sh
+++ day5/solution.sh
@@ -1,140 +1,63 @@
 #!/opt/homebrew/bin/bash
 
-declare -A stacks
-crate_index=(1 5 9 13 17 21 25 29 33)
+crate_char_index=(1 5 9 13 17 21 25 29 33)
 
-function print_stacks() {
-  echo "${stack_0[@]}"
-  echo "${stack_1[@]}"
-  echo "${stack_2[@]}"
-  echo "${stack_3[@]}"
-  echo "${stack_4[@]}"
-  echo "${stack_5[@]}"
-  echo "${stack_6[@]}"
-  echo "${stack_7[@]}"
-  echo "${stack_8[@]}"
-}
-
+stacks=()
 function put_in_stack() {
-  if [[ $i == 0 ]]; then
-    stack_0+=($crate)
-  fi
-  if [[ $i == 1 ]]; then
-    stack_1+=($crate)
-  fi
-  if [[ $i == 2 ]]; then
-    stack_2+=($crate)
-  fi
-  if [[ $i == 3 ]]; then
-    stack_3+=($crate)
-  fi
-  if [[ $i == 4 ]]; then
-    stack_4+=($crate)
-  fi
-  if [[ $i == 5 ]]; then
-    stack_5+=($crate)
-  fi
-  if [[ $i == 6 ]]; then
-    stack_6+=($crate)
-  fi
-  if [[ $i == 7 ]]; then
-    stack_7+=($crate)
-  fi
-  if [[ $i == 8 ]]; then
-    stack_8+=($crate)
-  fi
+  stacks[i]="${stacks[i]}${crate}"
 }
 
-function make_move() {
-  echo "FROM: ${from}"
-  if [[ $from == 0 ]]; then
-    to_move=${stack_0[@]:0:$qty}
-    stack_0=${stack_0[@]:$qty}
-    move_to
+function move_crates() {
+  from_stack="${stacks[from]}"
+  to_move=$(echo "${from_stack:0:$qty}")
+
+  if [ "$CRANE_MODEL" == "9000" ]; then
+    to_move=$(rev <<< $to_move)
   fi
-  if [[ $from == 1 ]]; then
-    to_move=${stack_1[@]:0:$qty}
-    stack_1=${stack_1[@]:$qty}
-    move_to
-  fi
-  if [[ $from == 2 ]]; then
-    to_move=${stack_2[@]:0:$qty}
-    stack_2=${stack_2[@]:$qty}
-    move_to
-  fi
-  if [[ $from == 3 ]]; then
-    stack_3+=($crate)
-  fi
-  if [[ $from == 4 ]]; then
-    stack_4+=($crate)
-  fi
-  if [[ $from == 5 ]]; then
-    stack_5+=($crate)
-  fi
-  if [[ $from == 6 ]]; then
-    stack_6+=($crate)
-  fi
-  if [[ $from == 7 ]]; then
-    stack_7+=($crate)
-  fi
-  if [[ $from == 8 ]]; then
-    stack_8+=($crate)
-  fi
+
+  stacks[from]=${from_stack:$qty}
+  to_stack="${stacks[to]}"
+  stacks[to]="${to_move}${to_stack}"
 }
 
-function move_to() {
-  for crate_to_move in $to_move; do
-    echo "moving ${crate_to_move}"
-      if [[ $to == 0 ]]; then
-        stack_0+=($crate_to_move)
-      fi
-      if [[ $to == 1 ]]; then
-        stack_1+=($crate_to_move)
-      fi
-      if [[ $to == 2 ]]; then
-        stack_2+=($crate_to_move)
-      fi
-      if [[ $to == 3 ]]; then
-        stack_3+=($crate_to_move)
-      fi
-      if [[ $to == 4 ]]; then
-        stack_4+=($crate_to_move)
-      fi
-      if [[ $to == 5 ]]; then
-        stack_5+=($crate_to_move)
-      fi
-      if [[ $to == 6 ]]; then
-        stack_6+=($crate_to_move)
-      fi
-      if [[ $to == 7 ]]; then
-        stack_7+=($crate_to_move)
-      fi
-      if [[ $to == 8 ]]; then
-        stack_8+=($crate_to_move)
-      fi
+function get_top_crates() {
+  for crates in ${stacks[@]}; do
+    echo ${crates:0:1}
   done
 }
 
 
 IFS=''
-while read -r line; do
-  print_stacks
-  if [[ $line =~ "[" ]]; then
-    for i in "${!crate_index[@]}"; do
-      char_count="${crate_index[$i]}"
-      crate="${line:char_count:1}"
-      if [[ "$crate" != " " ]]; then
-        put_in_stack
-      fi
-    done
-  fi
 
-  if [[ $line =~ "move" ]]; then
-    echo "doing move: ${line}"
-    qty=$(cut -d " " -f 2 <<< "$line")
-    from=$(( $(cut -d " " -f 4 <<< "$line") - 1 ))
-    to=$(( $(cut -d " " -f 6 <<< "$line") - 1 ))
-    make_move
-  fi
-done < sample_input.txt
-echo ${stack_0[@]}
+function solve() {
+  while read -r line; do
+    if [[ $line =~ "[" ]]; then
+      for i in "${!crate_char_index[@]}"; do
+        char_count="${crate_char_index[$i]}"
+        crate="${line:char_count:1}"
+        if [[ "$crate" != " " ]]; then
+          put_in_stack
+        fi
+      done
+    fi
+
+
+    if [[ $line =~ "move" ]]; then
+      qty=$(cut -d " " -f 2 <<< "$line")
+      from=$(( $(cut -d " " -f 4 <<< "$line") - 1 ))
+      to=$(( $(cut -d " " -f 6 <<< "$line") - 1 ))
+      move_crates
+    fi
+  done < input.txt
+}
+
+echo "part 1" #TDCHVHJTG
+CRANE_MODEL=9000
+solve
+get_top_crates
+
+echo "part 2" #TDCHVHJTG
+CRANE_MODEL=9001
+solve
+get_top_crates
+
